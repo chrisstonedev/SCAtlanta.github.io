00b3ba86e1834506941b66173896e54607ccb6e4
Chris Stone <contact@chrisstone.dev>
2023-05-29 20:48:58 -0400

    Fixing pathPrefix configuration

--- .eleventy.js
+++ .eleventy.js
@@ -16,5 +16,5 @@ module.exports = function (eleventyConfig) {
 		return isoDate ? `<time datetime="${isoDate}">${isoDate}</time>` : '';
 	});
 	eleventyConfig.setLibrary('md', md);
-	return {pathPrefix: 'SCAtlanta.github.io', dir: {input: 'src'}};
+	return {dir: {input: 'src'}};
 }
